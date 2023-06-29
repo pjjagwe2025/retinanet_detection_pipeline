f2e38b2c686d6d25773b712768098d8329b56dbe
sovit-123 <sovitrath5@gmail.com>
2023-06-29 06:01:39 +0530

    Removed redundant files

--- read_image.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-from PIL import Image
-
-# img = cv2.imread('data/STDW-main/STDW-main/images/108000_1.PNG')
-# print(img.shape)
-
-img = Image.open('data/STDW-main/STDW-main/images/108000_1.PNG')
-print(img.size)

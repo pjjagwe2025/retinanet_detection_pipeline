41b07d62ea8f2b84b3899abc23d5a26af6921e85
pjjagwe2025 <pjjagwe@vt.edu>
2025-07-29 18:35:19 -0400

    Update config.py

--- config.py
+++ config.py
@@ -10,35 +10,33 @@ DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 # Keep `resolutions=None` for not using multi-resolution training,
 # else it will be 50% lower than base `RESIZE_TO`, then base `RESIZE_TO`, 
 # and 50% higher than base `RESIZE_TO`
-RESOLUTIONS = [
-    (int(RESIZE_TO/2), int(RESIZE_TO/2)), 
-    (int(RESIZE_TO/1.777), int(RESIZE_TO/1.777)), 
-    (int(RESIZE_TO/1.5), int(RESIZE_TO/1.5)), 
-    (int(RESIZE_TO/1.333), int(RESIZE_TO/1.333)), 
-    (RESIZE_TO, RESIZE_TO), 
-    (int(RESIZE_TO*1.333), int(RESIZE_TO*1.333)), 
-    (int(RESIZE_TO*1.5), int(RESIZE_TO*1.5)), 
-    (int(RESIZE_TO*1.777), int(RESIZE_TO*1.777)), 
-    (int(RESIZE_TO*2), int(RESIZE_TO*2))
-]
-# RESOLUTIONS = None
+#RESOLUTIONS = [
+   # (int(RESIZE_TO/2), int(RESIZE_TO/2)), 
+   # (int(RESIZE_TO/1.777), int(RESIZE_TO/1.777)), 
+   # (int(RESIZE_TO/1.5), int(RESIZE_TO/1.5)), 
+   # (int(RESIZE_TO/1.333), int(RESIZE_TO/1.333)), 
+   # (RESIZE_TO, RESIZE_TO), 
+   # (int(RESIZE_TO*1.333), int(RESIZE_TO*1.333)), 
+   # (int(RESIZE_TO*1.5), int(RESIZE_TO*1.5)), 
+   # (int(RESIZE_TO*1.777), int(RESIZE_TO*1.777)), 
+   # (int(RESIZE_TO*2), int(RESIZE_TO*2))
+#]
+RESOLUTIONS = None
 
 # Training images and XML files directory.
-TRAIN_IMG = 'data/aquarium/train'
-TRAIN_ANNOT = 'data/aquarium/train'
+TRAIN_IMG = 'custom_data/train'
+TRAIN_ANNOT = 'custom_data/train'
 # Validation images and XML files directory.
-VALID_IMG = 'data/aquarium/valid'
-VALID_ANNOT = 'data/aquarium/valid'
+VALID_IMG = 'custom_data/valid'
+VALID_ANNOT = 'custom_data/valid'
+# Test images and XML files directory.
+TEST_IMG  = 'custom_data/test'
+TEST_ANNOT = 'custom_data/test'
 # Classes: 0 index is reserved for background.
 CLASSES = [
     '__background__',
-    'fish', 
-    'jellyfish', 
-    'penguin',
-    'shark', 
-    'puffin', 
-    'stingray',
-    'starfish'
+    'plant', 
+    'weed'
 ]
 
 NUM_CLASSES = len(CLASSES)
